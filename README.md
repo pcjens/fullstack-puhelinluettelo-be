f2f51b8edbfdf3f6cd5aab716513f00556c1f286
Jens Pitkanen <jens.pitkanen@helsinki.fi>
2018-11-17 22:14:04 +0200

    Add 3.1

---
+++ README.md
@@ -0,0 +1,4 @@
+# Puhelinluettelo backend
+Fullstack-kurssin [osan
+3](https://fullstackopen.github.io/teht%C3%A4v%C3%A4t/#osa-3)
+tehtävissä luotu backend.
---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+const API_PREFIX = '/api'
+
+let db = [
+  {
+    name: 'Arto Hellas',
+    number: '040-123456',
+    id: 1
+  },
+  {
+    name: 'Martti Tienari',
+    number: '040-123456',
+    id: 2
+  },
+  {
+    name: 'Arto Järvinen',
+    number: '040-123456',
+    id: 3
+  },
+  {
+    name: 'Lea Kutvonen',
+    number: '040-123456',
+    id: 4
+  }
+]
+
+app.get(API_PREFIX + '/persons', (req, res) => {
+  res.json(db)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
