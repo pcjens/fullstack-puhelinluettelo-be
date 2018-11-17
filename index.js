0c63ac345e9b4b0276804acd8d48e84434e0f50b
Jens Pitkanen <jens.pitkanen@helsinki.fi>
2018-11-17 22:41:57 +0200

    Add 3.4

--- index.js
+++ index.js
@@ -46,6 +46,12 @@ app.get(API_PREFIX + '/persons/:id', (req, res) => {
   }
 })
 
+app.delete(API_PREFIX + '/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  db = db.filter(person => person.id !== id)
+  res.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
